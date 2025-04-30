86db87a7c05a5bae80ccf4b37775af5319d69a3d
Bryant Emmanuel Wasian <Bryantwasian@gmail.com>
2025-04-30 11:25:57 +0700

    First changes

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+const bg1 = document.getElementsByClassName("bg1");
+const bg2 = document.getElementsByClassName("bg2");
+const bg3 = document.getElementsByClassName("bg3");
+let konversi_input1;
+
+document.getElementById("bg1").onclick = function(){
+    konversi_input1 = document.getElementById("konversi_input1").value;
+}
