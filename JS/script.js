3596cdbdd43bc941aa2861d9025a837ae3ae5780
Bryant Emmanuel Wasian <Bryantwasian@gmail.com>
2025-05-01 21:43:54 +0700

    Final changes

--- JS/script.js
+++ JS/script.js
@@ -10,21 +10,6 @@ let fahrenheit;
 let rumus1 = "* 9/5 + 32 = ";
 let rumus2 = "* 5/9 - 32 = ";
 
-/*convert.addEventListener("click", function(){
-    //checks if convert is pressed or not
-    buttonConvert = !buttonConvert; //change boolean value
-
-    if(buttonConvert){
-        document.getElementById("convert").onclick = function(){
-            celcius = document.getElementById("konversi_input1").value;
-            celcius = parseFloat(celcius);
-            fahrenheit = (celcius * 9 / 5) + 32;
-            console.log(celcius);
-            document.getElementById("result_input").textContent= fahrenheit;
-            document.getElementById("Calculate_detail").textContent=celcius + rumus1 + fahrenheit + "°F";
-         }
-    }
-});*/
 document.getElementById("convert").onclick = function(){
     celcius = document.getElementById("konversi_input1").value;
     celcius = parseFloat(celcius);
@@ -34,34 +19,11 @@ document.getElementById("convert").onclick = function(){
     document.getElementById("Calculate_detail").textContent=celcius + rumus1 + fahrenheit + "°F";
 };
 
-/*reset.addEventListener("click", function(){
-    buttonReset = !buttonReset;
-    if(buttonReset){
-        document.getElementById("reset").onclick = function(){
-            document.getElementById("konversi_input1").value = "";
-            document.getElementById("result_input").textContent = "";
-        }
-    };
-});*/
 document.getElementById("reset").onclick = function(){
     document.getElementById("konversi_input1").value = "";
     document.getElementById("result_input").textContent = "";
 }
 
-/*reverse.addEventListener("click", function(){
-    buttonReverse = !buttonReverse;
-    if(buttonReverse){
-        document.getElementById("reverse").onclick = function(){
-            fahrenheit = document.getElementById("konversi_input1").value;
-            fahrenheit = parseInt(fahrenheit);
-            document.getElementById("myLabel1").textContent = "Fahrenheit (°F)";
-            document.getElementById("myLabel2").textContent = "Celcius (°C)";
-            celcius = (fahrenheit * 5 / 9) -32;
-            document.getElementById("result_input").textContent= fahrenheit;
-            document.getElementById("Calculate_detail").textContent=celcius + rumus1 + fahrenheit + "°C";
-        }
-    }
-});*/
 document.getElementById("reverse").onclick = function(){
     fahrenheit = document.getElementById("konversi_input1").value;
     fahrenheit = parseInt(fahrenheit);
